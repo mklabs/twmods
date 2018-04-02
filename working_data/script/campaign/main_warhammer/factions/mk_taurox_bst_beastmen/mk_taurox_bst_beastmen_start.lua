41b9b871643c89d73767b345088daea7952f2425
Mickael Daniel <daniel.mickael@gmail.com>
2018-04-02 19:17:50 +0200

    Init new repo to include raw_data and working_data for scripts

---
+++ working_data/script/campaign/main_warhammer/factions/mk_taurox_bst_beastmen/mk_taurox_bst_beastmen_start.lua
@@ -0,0 +1,369 @@
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+--
+--  FACTION SCRIPT
+--
+--  Custom script for this faction starts here. The should_load_first_turn is
+--  queried to determine whether to load the startup script for the full-prelude
+--  first turn or just the standard startup script.
+--
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+
+local TAUROX_FORENAME = "names_name_709296598";
+local TAUROX_NAME = "names_name_1301160797";
+local TAUROX_FACTION = "mk_taurox_bst_beastmen";
+
+-- include the intro, prelude and quest chain scripts
+cm:load_faction_script(local_faction .. "_prelude");
+
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+--
+--  FACTION SCRIPT
+--
+--  This script sets up the default start camera (for a multiplayer game) and
+--  the intro cutscene/objective for a playable faction. The filename for this
+--  script must match the name of the faction.
+--
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+output("campaign script loaded for " .. local_faction);
+
+-------------------------------------------------------
+--  work out who starting general is
+-------------------------------------------------------
+
+starting_general_id = 0;    -- undefined
+
+cam_mp_start_x = 266.6;
+cam_mp_start_y = 204.8;
+cam_mp_start_d = 10;
+cam_mp_start_b = 0;
+cam_mp_start_h = 10;
+
+if (cm:is_new_game() and general_with_forename_exists_in_faction_with_force(local_faction, TAUROX_FORENAME))
+  or (cm:get_saved_value("starting_general_1") == TAUROX_FORENAME or cm:get_saved_value("starting_general_2") == TAUROX_FORENAME) then
+
+  starting_general_id = 1;
+  output("\tstarting_general_id is 1 (Taurox)");
+
+  -- Khazrak chapter missions, for now
+  if not cm:is_multiplayer() then
+    chapter_one_mission = chapter_mission:new(1, local_faction, "wh_dlc03_objective_beastmen_main_khazrak_01", nil);
+    chapter_two_mission = chapter_mission:new(2, local_faction, "wh_dlc03_objective_beastmen_main_khazrak_02", nil);
+    chapter_three_mission = chapter_mission:new(3, local_faction, "wh_dlc03_objective_beastmen_main_khazrak_03", nil);
+    chapter_four_mission = chapter_mission:new(4, local_faction, "wh_dlc03_objective_beastmen_main_khazrak_04", nil);
+    chapter_five_mission = chapter_mission:new(5, local_faction, "wh_dlc03_objective_beastmen_main_khazrak_05", nil);
+  end;
+else
+  script_error("ERROR: couldn't determine who starting lord is in Beastmen campaign, starting_general_1 in savegame is " .. tostring(cm:get_saved_value("starting_general_1")) .. " and starting_general_2 is " .. tostring(cm:get_saved_value("starting_general_2")));
+end;
+
+-- AI Beastmen armies can get stuck in encampment stance attempting to
+-- replenish losses that they suffer from low army morale attrition
+-- Mitch, 04/07/16
+function apply_effect_bundles()
+  output('Applying effect bundles to own faction for low morale immunity and animosity bonus');
+  cm:apply_effect_bundle("wh_dlc03_low_morale_attrition_immunity", TAUROX_FACTION, -1);
+  cm:apply_effect_bundle("wh2_main_bundle_greenskin_animosity_bonus", TAUROX_FACTION, -1);
+end;
+
+function add_beastmen_final_battle_listener()
+  if not cm:get_saved_value("bst_final_battle_quest") then
+    core:add_listener(
+      "Beastmen_Final_Battle",
+      "FactionTurnStart",
+      function(context)
+        local faction = context:faction();
+        return faction:is_human() and faction:name() == TAUROX_FACTION and are_all_beastmen_final_battle_factions_dead();
+      end,
+      function()
+        output('Trigger mission wh_dlc03_qb_bst_the_final_battle');
+        cm:trigger_mission(TAUROX_FACTION, "wh_dlc03_qb_bst_the_final_battle", true);
+        cm:set_saved_value("bst_final_battle_quest", true);
+      end,
+      false
+    );
+  end;
+end;
+
+function are_all_beastmen_final_battle_factions_dead()
+  local factions = {
+  --  "wh_main_teb_estalia"
+  };
+
+  for i = 1, #factions do
+    if not get_faction(factions[i]):is_dead() then
+      return false;
+    end;
+  end;
+
+  return true;
+end;
+
+function apply_beastmen_default_diplomacy()
+  -- if Empire/Bretonnia/Dwarfs are human controlled (i.e. MPC) then all options are available to Beastmen (except trade)
+  if cm:is_multiplayer() then
+    local emp = get_faction("wh_main_emp_empire");
+    local beast = get_faction(TAUROX_FACTION);
+    if beast and emp:is_human() and beast:is_human() then
+      cm:add_default_diplomacy_record("faction:" .. TAUROX_FACTION, "faction:wh_main_emp_empire", "all", true, true, true);
+    end;
+
+    local brt = get_faction("wh_main_brt_bretonnia");
+    if beast and brt:is_human() and beast:is_human() then
+      cm:add_default_diplomacy_record("faction:" .. TAUROX_FACTION, "faction:wh_main_brt_bretonnia", "all", true, true, true);
+    end;
+
+    local dwf = get_faction("wh_main_dwf_dwarfs");
+    if beast and dwf:is_human() and beast:is_human() then
+      cm:add_default_diplomacy_record("faction:" .. TAUROX_FACTION, "faction:wh_main_dwf_dwarfs", "all", true, true, true);
+    end;
+  end
+end;
+
+-------------------------------------------------------
+--  This gets called each time the script restarts,
+--  this could be at the start of a new game or
+--  loading from a save-game (including coming back
+--  from a campaign battle). Don't tamper with it.
+-------------------------------------------------------
+function start_game_for_faction(should_show_cutscene)
+  output("start_game_for_faction() called");
+
+  -- starts the playable faction script
+  fs_player:start(should_show_cutscene, true);
+end;
+
+-------------------------------------------------------
+--  This gets called after the intro cutscene ends,
+--  Kick off any missions or similar scripts here
+-------------------------------------------------------
+function start_faction()
+  output("start_faction() called");
+
+  -- show advisor progress button
+  cm:modify_advice(true);
+
+  start_beastmen_prelude();
+
+  if cm:is_multiplayer() == false then
+    show_how_to_play_event(local_faction);
+  end;
+end;
+
+-------------------------------------------------------
+--  This gets called only once - at the start of a
+--  new game. Initialise new game stuff for this
+--  faction here
+-------------------------------------------------------
+
+function faction_new_sp_game_startup()
+  output("faction_new_sp_game_startup() called");
+end;
+
+function faction_new_mp_game_startup()
+  output("faction_new_mp_game_startup() called");
+end;
+
+
+-------------------------------------------------------
+--  This gets called any time the game loads in,
+--  singleplayer including from a save game and
+--  from a campaign battle. Put stuff that needs
+--  re-initialising each campaign load in here
+-------------------------------------------------------
+
+function faction_each_sp_game_startup()
+  output("faction_each_sp_game_startup() called");
+
+  -- put stuff here to be initialised each time a singleplayer game loads
+
+  -- should we disable further advice
+  if cm:get_saved_value("advice_is_disabled") then
+    cm:set_advice_enabled(false);
+  end;
+end;
+
+function faction_each_mp_game_startup()
+  output("faction_each_mp_game_startup() called");
+  -- put stuff here to be initialised each time a multiplayer game loads
+end;
+
+-- DLC03 Beastmen Features
+output("==== Beastman Children of Chaos ====");
+add_beastmen_final_battle_listener();
+apply_beastmen_default_diplomacy();
+apply_effect_bundles();
+
+-------------------------------------------------------
+--  Faction Start declaration/config
+--  This object decides what to do when the faction
+--  is initialised - do we play the cutscene, do we
+--  position the camera at the start, or do we do
+--  nothing, stuff like that.
+--
+--  Comment out the line adding the intro cutscene
+--  to not play it (not ready for playtesting etc.)
+-------------------------------------------------------
+fs_player = faction_start:new(local_faction, cam_mp_start_x, cam_mp_start_y, cam_mp_start_d, cam_mp_start_b, cam_mp_start_h);
+-- singleplayer initialisation
+fs_player:register_new_sp_game_callback(function() faction_new_sp_game_startup() end);
+fs_player:register_each_sp_game_callback(function() faction_each_sp_game_startup() end);
+
+-- multiplayer initialisation
+fs_player:register_new_mp_game_callback(function() faction_new_mp_game_startup() end);
+fs_player:register_each_mp_game_callback(function() faction_each_mp_game_startup() end);
+
+if core:is_tweaker_set("DISABLE_PRELUDE_CAMPAIGN_SCRIPTS") then
+  output("Tweaker DISABLE_PRELUDE_CAMPAIGN_SCRIPTS is set so not running any prelude scripts");
+else
+  fs_player:register_intro_cutscene_callback(            -- comment out to not have intro cutscene
+    function()
+      show_benchmark_camera_pan_if_required(
+        function()
+          cutscene_intro_play();
+        end
+      );
+    end
+  );
+end;
+
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+--
+--  INTRO CUTSCENE
+--
+--  This function declares and configures the cutscene,
+--  loads it with actions and plays it.
+--  Customise it to suit.
+--
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+function cutscene_intro_play()
+  cutscene_intro_play_khazrak();
+end;
+
+
+function cutscene_intro_play_khazrak()
+  local cutscene_intro = campaign_cutscene:new(
+    local_faction .. "_intro_khazrak",          -- string name for this cutscene
+    94.5,                        -- length of cutscene in seconds
+    function() start_faction() end            -- end callback
+  );
+
+  local advice_to_play = {
+    "dlc03.camp.prelude.bst.intro.001",
+    "dlc03.camp.prelude.bst.intro.002",
+    "dlc03.camp.prelude.bst.intro.003",
+    "dlc03.camp.prelude.bst.intro.004",
+    "dlc03.camp.prelude.bst.intro.005"
+  };
+
+  --cutscene_intro:set_debug();
+  cutscene_intro:set_skippable(true, function() cutscene_intro_skipped(advice_to_play) end);
+  cutscene_intro:set_skip_camera(cam_mp_start_x, cam_mp_start_y, cam_mp_start_d, cam_mp_start_b, cam_mp_start_h);
+  cutscene_intro:set_disable_settlement_labels(false);
+
+  cutscene_intro:action(
+    function()
+      cm:show_shroud(false);
+      cm:set_camera_position(cam_mp_start_x, cam_mp_start_y, cam_mp_start_d, cam_mp_start_b, cam_mp_start_h);
+      cutscene_intro:cindy_playback("script/campaign/main_warhammer/factions/"..local_faction.."/scenes/beastmen_main_flyover_s01.CindyScene", true, true);
+    end,
+    0
+  );
+
+  -- "Tduigu-Uis" - a greeting in your tongue, my bestial Lord. I know you feel a yearning to kill me, for I am but a man, but you have received the vision, you know who has sent me… my desire is as yours - to see the Cloven Ones tear down all civilisation! So let us begin...
+  cutscene_intro:action(
+    function()
+      cm:show_advice(advice_to_play[1]);
+    end,
+    0.5
+  );
+  cutscene_intro:action(
+    function()
+      cutscene_intro:wait_for_advisor()
+    end,
+    24.5
+  );
+
+  -- To the north, past the mystic forest of Athel Loren, is Bretonnia. Arrogant and aloof, its glittering spires a sure sign of self-righteousness. You should bring ruin to this "pretty" realm.
+  cutscene_intro:action(
+    function()
+      cm:show_advice(advice_to_play[2]);
+    end,
+    25.0
+  );
+  cutscene_intro:action(
+    function()
+      cutscene_intro:wait_for_advisor()
+    end,
+    40.5
+  );
+
+  -- East of the Bretonnians lie the Empire, the very pinnacle of mankind's hubris. Its capital, Altdorf, is the centre of power and a symbol of its surety. Reduce it all to rubble - let pandemonium reign. The Empire is powerful, but divided. You must strike soon, for it may yet unify and seek to bring "civilisation" to the blood-grounds.
+  cutscene_intro:action(
+    function()
+      cm:show_advice(advice_to_play[3]);
+    end,
+    41
+  );
+  cutscene_intro:action(
+    function()
+      cutscene_intro:wait_for_advisor()
+    end,
+    67.5
+  );
+
+  -- To the south, the Border Princes hold a tentative grip on all that they call theirs, yet are isolated by the mountains and the Blackfire Pass - soft targets for a hungry herd such as ours.
+  cutscene_intro:action(
+    function()
+      cm:show_advice(advice_to_play[4]);
+    end,
+    68.0
+  );
+  cutscene_intro:action(
+    function()
+      cutscene_intro:wait_for_advisor()
+    end,
+    83
+  );
+
+  -- Destruction is in your blood, Nuis Ghurleth, and the world must know it. Sow as much as you can - the Cloven Ones shall destroy the world of man!
+  cutscene_intro:action(
+    function()
+      cm:show_advice(advice_to_play[5]);
+    end,
+    83.5
+  );
+
+  cutscene_intro:action(
+    function()
+      cutscene_intro:wait_for_advisor()
+    end,
+    94.5
+  );
+
+  cutscene_intro:action(
+    function()
+      cm:show_shroud(true);
+    end,
+    94.5
+  );
+
+  cutscene_intro:start();
+end;
+
+function cutscene_intro_skipped(advice_to_play)
+  cm:override_ui("disable_advice_audio", true);
+
+  effect.clear_advice_session_history();
+
+  for i = 1, #advice_to_play do
+    cm:show_advice(advice_to_play[i]);
+  end;
+
+  cm:callback(function() cm:override_ui("disable_advice_audio", false) end, 0.5);
+end;
---
+++ working_data/script/campaign/main_warhammer/mod/mk_taurox.lua
@@ -0,0 +1,110 @@
+local TAUROX_FORENAME = "names_name_709296598";
+local TAUROX_NAME = "names_name_1301160797";
+local TAUROX_FACTION = "mk_taurox_bst_beastmen";
+
+local starting_general_id = 0;		-- undefined
+
+function mk_taurox()
+  output("\n\nloaded mk TAUROX script\n\n");
+  output("Test name: " .. TAUROX_NAME);
+  output("Test forename: " .. TAUROX_FORENAME);
+  output("Test forename: " .. TAUROX_FACTION);
+  output("Local faction: " .. local_faction);
+
+  start();
+  ll_setup();
+  Add_Moon_Phase_Listeners(TAUROX_FACTION);
+end;
+
+-------------------------------------------------------
+--	work out who starting general is
+-------------------------------------------------------
+function start()
+  if (cm:is_new_game() and general_with_forename_exists_in_faction_with_force(local_faction, TAUROX_FORENAME))
+    or (cm:get_saved_value("starting_general_1") == TAUROX_FORENAME or cm:get_saved_value("starting_general_2") == TAUROX_FORENAME) then
+    -- Khazrak
+    starting_general_id = 1;
+    output("\tTAUROX starting_general_id is 1 (Taurox)");
+  else
+    output('Couldnt determine starting lord as Taurox, doing nothing as its most likely a vanilla LL');
+  end;
+
+  if cm:is_new_game() then
+    -- reposition starting lords for certain factions that require it
+    output("TAUROX Teleporting Beastmen starting Lords:");
+
+    -- Taurox...
+    -- move to Badlands area
+    output('TAUROX Reposition taurox');
+    reposition_starting_lord_for_faction(TAUROX_FACTION, TAUROX_FORENAME, TAUROX_FORENAME, 643, 191);
+
+    output('TAUROX Reposition ghorros towards taurox');
+    local taurox_is_chosen_lord = reposition_starting_lord_for_faction(TAUROX_FACTION, TAUROX_FORENAME, "names_name_2147357951", 629, 190); -- second army
+
+    if taurox_is_chosen_lord then
+      cm:force_declare_war("wh_main_grn_top_knotz", TAUROX_FACTION, false, false);
+    else
+      cm:force_declare_war("wh_main_teb_estalia", TAUROX_FACTION, false, false);
+    end;
+
+  else
+    output('TAUROX not a new game, not teleporting anything');
+  end;
+end;
+
+-- load the listeners for every human faction
+function ll_setup()
+  output('Setup legendary lord for ' .. TAUROX_FACTION);
+	local bst = get_faction(TAUROX_FACTION);
+
+	if bst and bst:is_human() then
+		for i = 1, #ll_beastmen do
+      output('Start legendary lord for ' .. TAUROX_FACTION);
+			ll_beastmen[i]:start();
+		end;
+	end;
+end;
+
+ll_beastmen = {
+	-- Khazrak
+	ll_unlock:new(
+		"wh_dlc03_bst_beastmen",
+		"2140784064",
+		"names_name_2147352487",
+		"CharacterPostBattleRelease",
+		function(context)
+			if context:character():faction():name() == TAUROX_FACTION then
+				local release_count = cm:get_saved_value("ll_beastmen_khazrak_release_count");
+				if release_count == nil then release_count = 0 end;
+
+				release_count = release_count + 1;
+
+				cm:set_saved_value("ll_beastmen_khazrak_release_count", release_count);
+
+				return release_count > 1;
+			end
+		end
+	),
+
+	-- Malagor
+	ll_unlock:new(
+		"wh_dlc03_bst_beastmen",
+		"2140784127",
+		"names_name_2147357619",
+		"MilitaryForceBuildingCompleteEvent",
+		function(context)
+			return context:building() == "wh_dlc03_horde_beastmen_arcane_1" and context:character():faction():name() == TAUROX_FACTION;
+		end
+	),
+
+	-- Morghur
+	ll_unlock:new(
+		"wh_dlc03_bst_beastmen",
+		"2140784189",
+		"names_name_2147352897",
+		"UnitCreated",
+		function(context)
+			return context:unit():unit_key() == "wh_dlc03_bst_mon_chaos_spawn_0" and context:unit():faction():name() == TAUROX_FACTION;
+		end
+	)
+};
