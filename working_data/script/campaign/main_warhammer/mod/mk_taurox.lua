1a6341efc31469c948dc84fc6b903cf329a5e62c
Mickael Daniel <daniel.mickael@gmail.com>
2018-04-05 00:29:58 +0200

    Updating skillt ree for Ghorros and updating scripts

--- working_data/script/campaign/main_warhammer/mod/mk_taurox.lua
+++ working_data/script/campaign/main_warhammer/mod/mk_taurox.lua
@@ -1,20 +1,19 @@
-local TAUROX_FORENAME = "names_name_709296598";
-local GHORROS_FORENAME = "names_name_2049222507";
-local TAUROX_FACTION = "mk_taurox_bst_beastmen";
-
-local starting_general_id = 0;		-- undefined
+local TAUROX_FORENAME = 'names_name_709296598';
+local GHORROS_FORENAME = 'names_name_2049222507';
+local TAUROX_FACTION = 'mk_taurox_bst_beastmen';
 
 function log(str)
   output('[taurox] ' .. str);
 end;
 
 function mk_taurox()
-  log("\n\nloaded mk TAUROX script\n\n");
-  log("Local faction: " .. local_faction);
+  log('\n\nloaded mk TAUROX script\n\n');
+  log('Local faction: ' .. local_faction);
 
-  mk_taurox_ll_setup();
   Add_Moon_Phase_Listeners(TAUROX_FACTION);
-  start();
+  local chosen_lord = start();
+  mk_taurox_ll_setup(chosen_lord);
+  mk_taurox_ll_lock(chosen_lord);
   scrollCameraToFactionLeader(function() log('Scrolled camera to faction leader') end);
 end;
 
@@ -23,43 +22,31 @@ end;
 -------------------------------------------------------
 function start()
   log('Start function called');
-
-  if (cm:is_new_game() and general_with_forename_exists_in_faction_with_force(local_faction, TAUROX_FORENAME))
-    or (cm:get_saved_value("starting_general_1") == TAUROX_FORENAME or cm:get_saved_value("starting_general_2") == TAUROX_FORENAME) then
-    -- Khazrak
-    starting_general_id = 1;
-    log("\tstarting_general_id is 1 (Taurox)");
-  else
-    log('Couldnt determine starting lord as Taurox, doing nothing as its most likely a vanilla LL');
+  if not cm:is_new_game() then
+    return;
   end;
 
-  if cm:is_new_game() then
-    -- reposition starting lords for certain factions that require it
-    log("Teleporting Beastmen starting Lords:");
+  log('Teleporting Beastmen starting Lords:');
 
-    -- Taurox...
-    -- move to Chaos wastelands area, Chaos start position
-    log('Reposition taurox');
-    reposition_starting_lord_for_faction(TAUROX_FACTION, TAUROX_FORENAME, TAUROX_FORENAME, 777, 600);
-    local taurox_is_chosen_lord = reposition_starting_lord_for_faction(TAUROX_FACTION, TAUROX_FORENAME, "names_name_2147357951", 773, 599); -- second army
+  -- Taurox... move to Chaos wastelands area, Chaos start position
+  log('Reposition taurox');
+  reposition_starting_lord_for_faction(TAUROX_FACTION, TAUROX_FORENAME, TAUROX_FORENAME, 777, 600);
+  local taurox_is_chosen_lord = reposition_starting_lord_for_faction(TAUROX_FACTION, TAUROX_FORENAME, 'names_name_2147357951', 773, 599); -- second army
 
-    -- Taurox...
-    -- move to Chaos wastelands area, Chaos start position
-    log('Reposition Ghorros');
-    reposition_starting_lord_for_faction(TAUROX_FACTION, TAUROX_FORENAME, TAUROX_FORENAME, 0, 0);
-    local ghorros_is_chosen_lord = reposition_starting_lord_for_faction(TAUROX_FACTION, TAUROX_FORENAME, "names_name_2147357951", -7, 6); -- second army
+  -- Ghorros... move to Morghur start position
+  log('Reposition Ghorros');
+  reposition_starting_lord_for_faction(TAUROX_FACTION, GHORROS_FORENAME, TAUROX_FORENAME, 508, 550);
+  local ghorros_is_chosen_lord = reposition_starting_lord_for_faction(TAUROX_FACTION, TAUROX_FORENAME, 'names_name_2147357951', 501, 556); -- second army
 
-    if taurox_is_chosen_lord then
-      cm:force_declare_war("wh_main_grn_top_knotz", TAUROX_FACTION, false, false);
-    elseif ghorros_is_chosen_lord then
-      cm:force_declare_war("wh2_main_def_naggarond", TAUROX_FACTION, false, false);
-    else
-      cm:force_declare_war("wh_main_teb_estalia", TAUROX_FACTION, false, false);
-    end;
-
+  if taurox_is_chosen_lord then
+    cm:force_declare_war('wh_main_grn_top_knotz', TAUROX_FACTION, false, false);
+  elseif ghorros_is_chosen_lord then
+    cm:force_declare_war('wh2_main_def_naggarond', TAUROX_FACTION, false, false);
   else
-    log('TAUROX not a new game, not teleporting anything');
+    cm:force_declare_war('wh_main_teb_estalia', TAUROX_FACTION, false, false);
   end;
+
+  return chosen_lord;
 end;
 
 -- load the listeners for every human faction
@@ -73,26 +60,37 @@ function mk_taurox_ll_setup()
 			ll_beastmen[i]:start();
 		end;
 	end;
+end;
 
+function mk_taurox_ll_lock(chosen_lord)
   if cm:is_new_game() then
-    log('Locking regular beastmen LL lords at the start of the game');
+    log('Locking LL lords at the start of the game');
     -- Khazrak
-    cm:lock_starting_general_recruitment("1902772535", TAUROX_FACTION);
+    cm:lock_starting_general_recruitment('1902772535', TAUROX_FACTION);
     -- Malagor
-    cm:lock_starting_general_recruitment("2072135186", TAUROX_FACTION);
+    cm:lock_starting_general_recruitment('2072135186', TAUROX_FACTION);
     -- Morghur
-    cm:lock_starting_general_recruitment("203069748", TAUROX_FACTION);
+    cm:lock_starting_general_recruitment('203069748', TAUROX_FACTION);
+    -- Taurox
+    if not chosen_lord == TAUROX_FORENAME then
+      cm:lock_starting_general_recruitment('1403603105', TAUROX_FACTION);
+    end;
+    -- Ghorros
+    if not chosen_lord == GHORROS_FORENAME then
+      cm:lock_starting_general_recruitment('681847135', TAUROX_FACTION);
+    end;
+    log('Locked LL lords');
   end;
 end;
 
 -- Credits to faction unlocker where this snippet comes from
 function scrollCameraToFactionLeader(done)
-  log("008 - Focus_Starting_Cameras");
+  log('008 - Focus_Starting_Cameras');
   if cm:is_new_game() then
-    log("008 - Focus_Starting_Cameras new game");
+    log('008 - Focus_Starting_Cameras new game');
     local faction = get_faction(local_faction);
     local faction_leader_cqi = faction:faction_leader():command_queue_index();
-    log("008 - cqi: " .. faction_leader_cqi);
+    log('008 - cqi: ' .. faction_leader_cqi);
     cm:scroll_camera_with_cutscene_to_character(6, done, faction_leader_cqi);
   end
 end;
@@ -102,17 +100,17 @@ ll_beastmen = {
 	 -- Khazrak
 	 ll_unlock:new(
 		TAUROX_FACTION,
-		"1902772535",
-		"names_name_1369338020",
-		"CharacterPostBattleRelease",
+		'1902772535',
+		'names_name_1369338020',
+		'CharacterPostBattleRelease',
 		function(context)
 			if context:character():faction():name() == TAUROX_FACTION then
-				local release_count = cm:get_saved_value("ll_beastmen_khazrak_release_count");
+				local release_count = cm:get_saved_value('ll_beastmen_khazrak_release_count');
 				if release_count == nil then release_count = 0 end;
 
 				release_count = release_count + 1;
 
-				cm:set_saved_value("ll_beastmen_khazrak_release_count", release_count);
+				cm:set_saved_value('ll_beastmen_khazrak_release_count', release_count);
 
 				return release_count > 9;
 			end
@@ -122,44 +120,44 @@ ll_beastmen = {
 	-- Malagor
 	ll_unlock:new(
 		TAUROX_FACTION,
-		"2072135186",
-		"names_name_435392760",
-		"MilitaryForceBuildingCompleteEvent",
+		'2072135186',
+		'names_name_435392760',
+		'MilitaryForceBuildingCompleteEvent',
 		function(context)
-			return context:building() == "wh_dlc03_horde_beastmen_gors_2" and context:character():faction():name() == TAUROX_FACTION;
+			return context:building() == 'wh_dlc03_horde_beastmen_gors_2' and context:character():faction():name() == TAUROX_FACTION;
 		end
 	),
 
 	-- Morghur
 	ll_unlock:new(
 		TAUROX_FACTION,
-		"203069748",
-		"names_name_1871285548",
-		"UnitCreated",
+		'203069748',
+		'names_name_1871285548',
+		'UnitCreated',
 		function(context)
-			return context:unit():unit_key() == "wh_dlc03_bst_mon_chaos_spawn_0" and context:unit():faction():name() == TAUROX_FACTION;
+			return context:unit():unit_key() == 'wh_dlc03_bst_mon_chaos_spawn_0' and context:unit():faction():name() == TAUROX_FACTION;
 		end
 	),
 
 	-- Taurox
 	ll_unlock:new(
 		TAUROX_FACTION,
-		"1403603105",
-		"names_name_709296598",
-		"MilitaryForceBuildingCompleteEvent",
+		'1403603105',
+		'names_name_709296598',
+		'MilitaryForceBuildingCompleteEvent',
 		function(context)
-			return context:building() == "wh_dlc03_horde_beastmen_minotaurs_1" and context:character():faction():name() == TAUROX_FACTION;
+			return context:building() == 'wh_dlc03_horde_beastmen_minotaurs_1' and context:character():faction():name() == TAUROX_FACTION;
 		end
 	),
 
 	-- Ghorros
 	ll_unlock:new(
 		TAUROX_FACTION,
-		"681847135",
-		"names_name_2049222507",
-    "MilitaryForceBuildingCompleteEvent",
+		'681847135',
+		'names_name_2049222507',
+    'MilitaryForceBuildingCompleteEvent',
 		function(context)
-			return context:unit():unit_key() == "wh_dlc03_horde_beastmen_centigors" and context:unit():faction():name() == TAUROX_FACTION;
+			return context:unit():unit_key() == 'wh_dlc03_horde_beastmen_centigors' and context:unit():faction():name() == TAUROX_FACTION;
 		end
 	),
 };
